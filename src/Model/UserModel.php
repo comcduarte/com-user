a1c1a753eb5e1b73d157e1ba0df348078c733cf9
DuarteC <DuarteC@IT-WKS010.MidNet.CityOfMiddletown.com>
2020-04-09 09:37:36 -0400

    Removed assignedAction function

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -5,10 +5,8 @@ use Components\Model\AbstractBaseModel;
 use Laminas\Crypt\Password\Bcrypt;
 use Laminas\Db\Sql\Delete;
 use Laminas\Db\Sql\Insert;
-use Laminas\Db\Sql\Select;
 use Laminas\Db\Sql\Sql;
 use Laminas\Validator\Identical;
-use User\Form\UserRolesForm;
 use Exception;
 
 
@@ -98,71 +96,6 @@ class UserModel extends AbstractBaseModel
             return $e;
         }
         return $this;
-    }
-    
-    public function assignAction()
-    {
-        $uuid = $this->params()->fromRoute('uuid', 0);
-        
-        //-- Create User Model from Record --//
-        $this->model->read(['UUID' => $uuid]);
-        
-        //-- Create UserRolesForm --//
-        $form = new UserRolesForm();
-        $form->setDbAdapter($this->adapter);
-        $form->setUser(['UUID' => $uuid]);
-        $form->init();
-        $form->get('SUBMIT')->setAttribute('value', 'Add');
-        
-        
-        
-        
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            //-- Capture POST --//
-            $form->setInputFilter($model->getInputFilter());
-            $form->setData($request->getPost());
-            
-            if ($form->isValid()) {
-                $this->model->assignRole($form->getData('ROLE'));
-                $this->redirect()->toRoute('user/default');
-            }
-        }
-        
-        //-- BEGIN: Retrieve currently assigned roles --//
-        $sql = new Sql($this->adapter);
-        
-        $select = new Select();
-        $select->from('user_roles');
-        $select->where(['USER' => $uuid]);
-        
-        $statement = $sql->prepareStatementForSqlObject($select);
-        
-        try {
-            $resultSet = $statement->execute();
-        } catch (Exception $e) {
-            return $e;
-        }
-        
-        $roles = [];
-        foreach ($resultSet as $role) {
-            $rolemodel = new RoleModel($this->adapter);
-            $rolemodel->read(['UUID' => $role['ROLE']]);
-            $roles[] = [
-                'ROLENAME' => $rolemodel->ROLENAME,
-                'ROLEUUID' => $rolemodel->UUID,
-                'UUID' => $role['UUID'],
-            ];
-        }
-        //-- END: Retrieve currently assigned roles --//
-        
-        
-        return ([
-            'form' => $form,
-            'username' => $model->USERNAME,
-            'user-uuid' => $model->UUID,
-            'roles' => $roles,
-        ]);
     }
     
     public function unassignRole($data)
